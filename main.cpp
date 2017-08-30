1182a727a6084977aa2c802d5e053e4913347008
Viktor <vik24rus@gmail.com>
2017-08-30 20:06:20 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -418,10 +418,9 @@ int main()
     cout << "sucess" << endl;
 }*/
 
-/*Rand 2
-// * Заполнить массив из 50-ти элементов нечётными числами от 1 до 99.
-// * (используйте операцию остаток от деления, чтобы проверить число на чётность)
-*/
+/* Заполнить массив из 50-ти элементов нечётными числами от 1 до 99.
+// (используйте операцию остаток от деления, чтобы проверить число на чётность)
+
 #include <iostream>
 #include <time.h>
 #include <cstdlib>
@@ -437,13 +436,10 @@ int main()
 
     for(int i=0; i <50 ; i++)
     {
-        rand_tmp = 1 + rand() % (99-1+1);
-        if (rand_tmp % 2 != 0){
+        do{
             rand_tmp = 1 + rand() % (99-1+1);
-        }else
-
-
-        mass[i] = rand_tmp // генератор от 7 до 14 - включая оба числа. lowerLimit + rand() % (upperLimit - lowerLimit + 1);
+        }while (rand_tmp % 2 == 0);
+        mass[i] = rand_tmp; // генератор от 7 до 14 - включая оба числа. lowerLimit + rand() % (upperLimit - lowerLimit + 1);
     }
 
     for(int i=0; i <50 ; i++)
@@ -451,4 +447,394 @@ int main()
         cout << mass[i]  << "  " << endl;
     }
     cout << "WORKED" << endl;
+}
+*/
+
+/* Объявить три массива. Первые два заполнить случайными значениями от 10 до 30.
+В элементы третьего массива записать сумму соответствующих элементов первых двух массивов.
+(в нулевую ячейку третьего — сумму нулевых ячеек первого и второго массивов и так далее).
+Затем найти среднее арифметическое элементов третьего массива,
+максимальное значение и минимальное значение, которые он хранит.
+
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main()
+{
+    int mass3_sred = 0;
+    int mass3_max = 0;
+    int mass3_min = 30 ;
+    int mass1[50];
+    int mass2[50];
+    int mass3[50];
+
+    srand(time(NULL));
+
+    for(int i=0; i <50 ; i++)
+    {
+       mass1[i] = 10 + rand() % (30-10+1);
+       mass2[i] = 10 + rand() % (30-10+1);
+       mass3[i] = mass1[i]+mass2[i];
+    }
+
+
+    cout << "1 mass " << endl;
+
+    for(int i=0; i <50 ; i++)
+    {
+        cout << mass1[i]  << "  " ;
+    }
+
+    cout << endl;
+    cout << "2 mass " << endl;
+
+    for(int i=0; i <50 ; i++)
+    {
+        cout << mass2[i]  << "  " ;
+    }
+
+    cout << endl;
+    cout << "3 mass " << endl;
+
+    for(int i=0; i <50 ; i++)
+    {
+        cout << mass3[i]  << "  " ;
+    }
+
+    cout << endl;
+
+    for(int i=0; i <50 ; i++)
+    {
+       if( mass3[i] > mass3_max){
+           mass3_max = mass3[i];
+       }
+       if( mass3[i] < mass3_min){
+           mass3_min = mass3[i];
+       }
+       mass3_sred+= mass3[i];
+    }
+
+    cout << "Среднее арифметическое 3-го: " << mass3_sred/30 << endl;
+    cout << "Мин 3-го: " << mass3_min << endl;
+    cout << "Макс 3-го: " << mass3_max << endl;
+}
+*/
+
+/* Заполнить двумерный массив случайными числами от 10 до 100.
+ * Посчитать сумму элементов отдельно в каждой строке и определить номер строки, в которой эта сумма максимальна.
+ *
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main()
+{
+    int mass[5][5] ;
+    int summ_str = 0;
+    int max = 0;  //макс значение в строке
+    int nomer_max =0 ;  //номер строки с максимальным значением
+
+
+    srand(time(NULL));
+
+    for(int i=0; i < 5 ; i++)
+    {
+       for(int j=0;j< 5;j++){
+           mass[i][j] = 10 + rand() % (100-10+1); // генератор от 10 до 100  lowerLimit + rand() % (upperLimit - lowerLimit + 1);
+
+       }
+    }
+
+    for(int i=0; i < 5 ; i++)   // вывод че нам сгенерировалося
+    {
+       for(int j=0; j< 5;j++){
+           cout << mass[i][j] << " ";
+       }
+       cout << endl;
+    }
+
+    for(int i=0; i < 5 ; i++)
+    {
+        for(int j=0; j< 5;j++)
+        {
+                summ_str += mass[i][j];
+                if (mass[i][j] > max)
+                {
+                    max = mass[i][j];
+                    nomer_max = i;
+                }
+                if (j == 4) //когда цикл обошел все столбцы в одной строке
+                {
+                    cout << "Сумма элементов в строке" << i+1 << " " << summ_str << endl ;
+                    summ_str = 0;
+                }
+
+
+        }
+    }
+    cout << "В строке " << nomer_max+1 << " максимальное значение " << max << endl ;
+
+}
+*/
+
+/*
+//Написать игру в которой имитируется бросание кубиков компьютером и пользователем.
+//В игре 2 кубика и на каждом из них может выпасть от 1 до 6 очков. Реализовать определение программой первого ходящего.
+//Каждый делает по четыре броска. После бросков показать, нарисованные символами кубики и количество очков, выпавших на них.
+//После пары бросков (бросок компьютера + бросок пользователя) выводить на экран промежуточный результат
+//— количество набранных очков игроком и компьютером.
+//В конце сообщить о том, кто выиграл по итогам всех бросков.
+// 1 + rand() % (6-1+1 ) ====  lowerLimit + rand() % (upperLimit - lowerLimit + 1);
+
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+bool menu = true;   //для цикла меню
+int menu_control = 0;  //для выбора в меню
+bool firstPlayHuman =true;
+int playerVvod = 0;  //ввод игрока
+
+int cubic_1 = 0; //значение выпавшее на кубике 1
+int cubic_2 = 0; //значение выпавшее на кубике 2
+
+
+int ochkiHuman[4]; //очки за каждый ход у человека
+int ochkiHumanSumm=0; //для промежуточной суммы и в конце для итоговой суммы очков
+int ochkiPC[4]; //очки за каждый ход у компьютера
+int ochkiPCSumm=0; //для промежуточной суммы и в конце для итоговой суммы очков
+
+void render(int i , string player); //нарисовать картинку + подписать
+void run(string player); //основной цикл с игрой в 4 хода
+void resultItog(); //итоговая
+void result(); //промежуточная сумма
+
+int mass[4] = {1,1,1,1};
+int tmp=0;
+int main()
+{
+//    for (int i=0; i < 4; i++){
+//        tmp+=mass[i];
+//    }
+//    cout << tmp;
+    srand(time(NULL));
+    cout << "Игра в кости" << endl << "1. Выбор первого игрока" << endl << "2. Начать игру" << endl << "3. Выход" << endl;
+    do{
+        cin >> menu_control;
+        switch (menu_control) {
+        case 1:
+            cout << "Если Вы хотите ходить первым нажмите 1 (Первый ход игрок) , иначе нажмите 2 (Первый ход ПК)" << endl;
+            cin >> playerVvod;
+            if(playerVvod == 1)
+            {
+                firstPlayHuman=true;
+                cout << "Первым ходит человек" << endl;
+            }else
+            {
+                firstPlayHuman=false;
+                cout << "Первым ходит компьютер" << endl;
+            }
+            cout << endl << "Игра в кости" << endl << "1. Выбор первого игрока" << endl << "2. Начать игру" << endl << "3. Выход" << endl;
+            break;
+        case 2:
+            cout << "Игра началась" << endl;
+            if(firstPlayHuman){
+                cout << "Первым ходит человек" << endl;
+                run("human");
+            }else{
+                cout << "Первым ходит компьютер" << endl;
+                run("pc");
+            }
+            resultItog();
+            break;
+        case 3:
+            cout << "До свидания" << endl;
+            menu = false;
+            break;
+
+        default:
+            cout << "Не корректный ввод" << endl;
+            break;
+        }
+
+    }while(menu == true);
+
+}
+
+void run(string player)
+{
+    for(int i =0 ; i < 4; i++)
+    {
+        if (player == "human") //первым ход человека
+        {
+            cout << "Бросаем кубик? (1 для броска)" << endl;
+            cin >> playerVvod;
+            if(playerVvod == 1)
+            {
+                cubic_1 = 1 + rand() % (6-1+1 );
+                cubic_2 = 1 + rand() % (6-1+1 );
+                ochkiHuman[i] = cubic_1 + cubic_2;
+                render(cubic_1, "игрок");
+                render(cubic_2, "игрок");
+
+
+                cout << "-----------------ХОД КОМПЬЮЕТРА!!!!!-----------"<< endl;
+                cubic_1 = 1 + rand() % (6-1+1 );
+                cubic_2 = 1 + rand() % (6-1+1 );
+                ochkiPC[i] = cubic_1 + cubic_2;
+                render(cubic_1, "компьютер");
+                render(cubic_2, "компьютер");
+                result();
+            }else
+            {
+                cout << "Игра в кости" << endl << "1. Выбор первого игрока" << endl << "2. Начать игру" << endl << "3. Выход" << endl;
+            }
+
+
+        }else              //первым ход компьютера
+        {
+            cout << "-----------------ХОД КОМПЬЮЕТРА!!!!!-----------" << endl;
+            cubic_1 = 1 + rand() % (6-1+1 );
+            cubic_2 = 1 + rand() % (6-1+1 );
+            ochkiPC[i] = cubic_1 + cubic_2;
+            render(cubic_1, "компьютер");
+            render(cubic_2, "компьютер");
+            result();
+
+            cout << "Бросаем кубик? (1 для броска)" << endl;
+            cin >> playerVvod;
+            if(playerVvod == 1)
+                {
+                    cubic_1 = 1 + rand() % (6-1+1 );
+                    cubic_2 = 1 + rand() % (6-1+1 );
+                    ochkiHuman[i] = cubic_1 + cubic_2;
+                    render(cubic_1, "игрок");
+                    render(cubic_2, "игрок");
+                }else
+                {
+                    cout << "Игра в кости" << endl << "1. Выбор первого игрока" << endl << "2. Начать игру" << endl << "3. Выход" << endl;
+                }
+
+        }
+    }
+}
+
+void result(){
+    for(int i=0; i < 4 ; i++)
+    {
+        ochkiHumanSumm += ochkiHuman[i];
+        ochkiPCSumm += ochkiPC[i];
+    }
+    cout << "------------------" << endl;
+    cout << "У вас очков " << ochkiHumanSumm << endl;
+    cout << "У компьютера очков " << ochkiPCSumm << endl;
+    cout << "------------------" << endl;
+    ochkiPCSumm =0;
+    ochkiHumanSumm=0;
+}
+
+void resultItog(){
+    for(int i=0; i < 4 ; i++)
+    {
+        ochkiHumanSumm += ochkiHuman[i];
+        ochkiPCSumm += ochkiPC[i];
+    }
+    if(ochkiHuman > ochkiPC){
+        cout << "Вы победили! " << "со счетом " << ochkiHumanSumm << " против " <<  ochkiPCSumm << endl;
+        menu = false;
+    }else
+    {
+        cout << "Вы проиграли! " << "со счетом " << ochkiHumanSumm << " против " <<  ochkiPCSumm << endl;
+        menu = false;
+    }
+}
+
+void render(int i , string player){
+    switch (i) {
+    case 1:
+        cout << "-----------------"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|       X       |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "-----------------"<< endl;
+        cout << "У " << player << "а выпал КУБИК " << 1 << endl;
+
+        break;
+    case 2:
+        cout << "-----------------"<< endl;
+        cout << "|            X  |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|  X            |"<< endl;
+        cout << "-----------------"<< endl;
+        cout << "У " << player << "а выпал КУБИК " << 2 << endl;
+        break;
+    case 3:
+        cout << "-----------------"<< endl;
+        cout << "|            X  |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|       X       |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|  X            |"<< endl;
+        cout << "-----------------"<< endl;
+        cout << "У " << player << "а выпал КУБИК " << 3 << endl;
+        break;
+    case 4:
+        cout << "-----------------"<< endl;
+        cout << "|  X         X  |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|  X         X  |"<< endl;
+        cout << "-----------------"<< endl;
+        cout << "У " << player << "а выпал КУБИК " << 4 << endl;
+        break;
+    case 5:
+        cout << "-----------------"<< endl;
+        cout << "|  X         X  |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|       X       |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|  X         X  |"<< endl;
+        cout << "-----------------"<< endl;
+        cout << "У " << player << "а выпал КУБИК " << 5 << endl;
+        break;
+    case 6:
+        cout << "-----------------"<< endl;
+        cout << "|  X         X  |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|  X         X  |"<< endl;
+        cout << "|               |"<< endl;
+        cout << "|  X         X  |"<< endl;
+        cout << "-----------------"<< endl;
+        cout << "У " << player << "а выпал КУБИК " << 6 << endl;
+        break;
+
+    }
+
+}
+*/
+
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main()
+{
+
 }
